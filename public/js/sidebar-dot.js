4b70e72b85d46c22944455cc4ec3e0d10f54c2f4
Len_AshBell <jason1022liu@gmail.com>
2018-08-06 03:10:32 +0800

    fix sidebar background showing

--- public/js/sidebar-dot.js
+++ public/js/sidebar-dot.js
@@ -121,7 +121,7 @@ $(function () {
   }
   $('canvas').on('mousemove', function (e) {
     mousePosition.x = e.pageX;
-    mousePosition.y = e.pageY - 400;
+    mousePosition.y = e.pageY;
   });
   $('canvas').on('mouseleave', function (e) {
     mousePosition.x = canvas.width / 2;
