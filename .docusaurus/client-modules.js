46e42f26d259e50f5a34fd02660bd35ee5a6d276
JasonXDDD <jason1022liu@gmail.com>
2021-10-26 10:57:30 +0800

    update new framework

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/bigdata/Documents/jasonxddd.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/bigdata/Documents/jasonxddd.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/bigdata/Documents/jasonxddd.github.io/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/bigdata/Documents/jasonxddd.github.io/src/css/custom.css"),
+];
